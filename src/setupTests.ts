2a99b24c5a7039774e6e2b1a9c1744c70f512119
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2025-08-31 12:21:43 +0100

    resolve htmlELement error not implemented

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -11,3 +11,21 @@ afterAll(() => server.close());
 import { vi } from 'vitest';
 // @ts-expect-error config setup
 globalThis.jest = vi;
+
+// Error: Not implemented: HTMLMediaElement.prototype.play/pause
+// https://github.com/jsdom/jsdom/issues/2155#issuecomment-366703395
+
+window.HTMLMediaElement.prototype.load = () => {
+  /* do nothing */
+};
+// @ts-expect-error config setup
+window.HTMLMediaElement.prototype.play = () => {
+  /* do nothing */
+};
+window.HTMLMediaElement.prototype.pause = () => {
+  /* do nothing */
+};
+// @ts-expect-error config setup
+window.HTMLMediaElement.prototype.addTextTrack = () => {
+  /* do nothing */
+};
